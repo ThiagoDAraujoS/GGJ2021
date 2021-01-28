c1b2848f0b155bb03015fc59f2ad2653f8b47a4c
Gary <gtexmo@gmail.com>
2021-01-28 08:23:00 -0700

    Basic item pickup. Need to merge with Thiago's controller and figure some other stuff out.

--- Assets/Scripts/BasicCharacterController.cs
+++ Assets/Scripts/BasicCharacterController.cs
@@ -5,53 +5,74 @@ using UnityEngine.InputSystem;
 public class BasicCharacterController : MonoBehaviour
 {
 	public float speed = 5;
+	public Collider2D col2d;
 
 	private InputMap controls;
-	private Vector2 _movement = Vector2.zero;
+	private Vector2 _movement;
+	private Collectable _touchedCollectable = null;
 
 	void Awake()
 	{
 		controls = new InputMap();
+	}
 
+	private void Update()
+	{
+		transform.position = new Vector3(transform.position.x, transform.position.y, col2d.bounds.max.y);
+	}
+
+	void FixedUpdate()
+	{
+		transform.position = transform.position + new Vector3(_movement.x, _movement.y) * speed * Time.deltaTime;
 	}
 
 	private void OnEnable()
 	{
-		controls.Enable();
-		controls.Player.Move.started += OnMoveStart;
+		controls.Player.Move.performed += OnMoveStart;
 		controls.Player.Move.canceled += OnMoveEnd;
+		controls.Player.Fire.performed += OnFire;
+		controls.Enable();
 	}
 
 	private void OnDisable()
 	{
 		controls.Disable();
-		controls.Player.Move.started -= OnMoveStart;
+		controls.Player.Move.performed -= OnMoveStart;
 		controls.Player.Move.canceled -= OnMoveEnd;
 	}
-
-	//void FixedUpdate()
-	//{
-	//	//transform.position = transform.position + new Vector3(_movement.x, _movement.y) * speed * Time.deltaTime;
-	//}
-
 
 	void OnMoveStart(InputAction.CallbackContext context)
 	{
 		_movement = context.ReadValue<Vector2>();
-		Debug.Log("MoveStart " + _movement);
 	}
 
 	void OnMoveEnd(InputAction.CallbackContext context)
 	{
 		_movement = Vector2.zero;
-		Debug.Log("MoveEnd " + _movement);
 	}
 
-	//void OnMove(InputAction.CallbackContext context)
-	//{
-	//	var input = context.ReadValue<Vector2>().normalized;
-	//	Debug.Log("YEP! " + input);
+	void OnFire(InputAction.CallbackContext context)
+	{
+		if (_touchedCollectable != null)
+		{
+			_touchedCollectable.col2d.isTrigger = true;
+			_touchedCollectable.gameObject.transform.SetParent(this.gameObject.transform);
+			
+			// TODO: Need to move this transform to be relative to the hands object?
+		}
+	}
 
-	//	//_movement = context.ReadValue<Vector2>();
-	//}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		// TODO: Maybe change this to a separate collider trigger so we can pick up nearby things, not just things we've smashed our face into?
+		// Maybe a circle collider?
+		var collectable = collision.gameObject.GetComponent<Collectable>();
+		if (collectable != null)
+			_touchedCollectable = collectable;
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		_touchedCollectable = null;
+	}
 }
--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,20 +2,22 @@
 
 public class Collectable : MonoBehaviour
 {
-	public BoxCollider2D c;
+	public BoxCollider2D col2d;
 
     void Awake()
     {
-        
     }
 
-    void Update()
-    {
-        
-    }
-
-	private void OnCollisionEnter2D(Collision2D collision)
+	private void Update()
 	{
-		Debug.Log("Collectable Touched");
+		BasicCharacterController character = this.transform.parent?.gameObject.GetComponent<BasicCharacterController>();
+		if (character != null)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, character.col2d.bounds.max.y - 0.1f);
+		}
+		else
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, col2d.bounds.max.y);
+		}
 	}
 }
