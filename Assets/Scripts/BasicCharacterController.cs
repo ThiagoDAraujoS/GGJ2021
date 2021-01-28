2e064c7cacd27617eada1915374c4285def434f4
Gary <gtexmo@gmail.com>
2021-01-28 00:20:17 -0700

    Trying to get a basic character controller working for debugging purposes.

---
+++ Assets/Scripts/BasicCharacterController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasicCharacterController : MonoBehaviour
+{
+	public float speed = 5;
+
+	private InputMap controls;
+	private Vector2 _movement = Vector2.zero;
+
+	void Awake()
+	{
+		controls = new InputMap();
+
+	}
+
+	private void OnEnable()
+	{
+		controls.Enable();
+		controls.Player.Move.started += OnMoveStart;
+		controls.Player.Move.canceled += OnMoveEnd;
+	}
+
+	private void OnDisable()
+	{
+		controls.Disable();
+		controls.Player.Move.started -= OnMoveStart;
+		controls.Player.Move.canceled -= OnMoveEnd;
+	}
+
+	//void FixedUpdate()
+	//{
+	//	//transform.position = transform.position + new Vector3(_movement.x, _movement.y) * speed * Time.deltaTime;
+	//}
+
+
+	void OnMoveStart(InputAction.CallbackContext context)
+	{
+		_movement = context.ReadValue<Vector2>();
+		Debug.Log("MoveStart " + _movement);
+	}
+
+	void OnMoveEnd(InputAction.CallbackContext context)
+	{
+		_movement = Vector2.zero;
+		Debug.Log("MoveEnd " + _movement);
+	}
+
+	//void OnMove(InputAction.CallbackContext context)
+	//{
+	//	var input = context.ReadValue<Vector2>().normalized;
+	//	Debug.Log("YEP! " + input);
+
+	//	//_movement = context.ReadValue<Vector2>();
+	//}
+}
---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+	public BoxCollider2D c;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collectable Touched");
+	}
+}
