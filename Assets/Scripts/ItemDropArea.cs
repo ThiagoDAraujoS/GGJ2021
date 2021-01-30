46fbb29070c8561b734e7ee587cfe03d4e41afad
Gary <gtexmo@gmail.com>
2021-01-30 09:49:13 -0700

    Added a DebugVariables property to GameManager that can have individual variables for various debug options that can all be disabled if desired. If not in debug mode, ItemDropArea will disable its sprite when the game is run.

--- Assets/Scripts/ItemDropArea.cs
+++ Assets/Scripts/ItemDropArea.cs
@@ -2,6 +2,14 @@
 
 public class ItemDropArea : MonoBehaviour
 {
+	public SpriteRenderer editorDisplayRenderer;
+
 	[HideInInspector]
 	public Collectible item = null;
+
+	private void Awake()
+	{
+		if (editorDisplayRenderer != null && !GameManager.DebugVariables.DropAreaEditorVisualsEnabled)
+			editorDisplayRenderer.enabled = false;
+	}
 }
