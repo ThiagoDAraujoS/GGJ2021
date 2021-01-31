bb27ddd53f82df217d315fa92e15ede79f5a4198
Gary <gtexmo@gmail.com>
2021-01-31 14:44:34 -0700

    Scene fades in and you can restart it.

---
+++ Assets/Scripts/UI/FadeInUIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInUIController : MonoBehaviour
+{
+	private class Constants
+	{
+		public const float FadeTime = 0.5f;
+	}
+
+	public Image MaskImage;
+
+    void Start()
+    {
+		StartCoroutine(HandleFadeOut());
+    }
+
+	private IEnumerator HandleFadeOut()
+	{
+		this.MaskImage.CrossFadeAlpha(0f, Constants.FadeTime, false);
+		yield return new WaitForSeconds(Constants.FadeTime);
+		DestroyImmediate(this.gameObject);
+	}
+}
